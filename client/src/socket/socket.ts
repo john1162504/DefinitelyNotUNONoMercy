a41db0e14f3cf81651ac2802c84782549b0811ae
John Chan <cch235@uclive.ac.nz>
2025-07-10 16:57:29 +1200

    fixed incorrect address

--- client/src/socket/socket.ts
+++ client/src/socket/socket.ts
@@ -2,7 +2,7 @@ import { io } from "socket.io-client";
 
 const SERVER_URL = import.meta.env.DEV
     ? "http://localhost:3001"
-    : "https://uno-backend.onrender.com";
+    : "https://definitelynotunonomercy.onrender.com";
 
 export const socket = io(SERVER_URL, { transports: ["websocket"] });
 export default socket;
